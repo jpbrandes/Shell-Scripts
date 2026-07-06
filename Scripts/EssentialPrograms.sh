119d3c05225017b7bed032b08b7b92a75e7e71a3
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 10:47:50 -0300

    Update script messages to English

--- Scripts/EssentialPrograms.sh
+++ Scripts/EssentialPrograms.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "ATUALIZANDO SISTEMA..."
+echo "UPDATING SYSTEM..."
 sudo apt update && sudo apt upgrade -y
 
-echo "INSTALANDO PROGRAMAS ESSENCIAIS"
+echo "INSTALLING ESSENTIAL PROGRAMS"
 sudo apt install -y vlc libreoffice gimp
 
-echo "INSTALANDO GOOGLE CHROME..."
+echo "INSTALLING GOOGLE CHROME..."
 # Baixa como usuário normal direto da internet
 # O -y nos comandos significa que é para aceitar direto
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
