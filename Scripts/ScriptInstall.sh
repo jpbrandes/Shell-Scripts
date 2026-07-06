61bcaadc905ee0da60f89f35f28227458f3fb64d
João Pedro <jpdevprojects879@gmail.com>
2026-07-06 10:48:48 -0300

    Update installation script to English messages

--- Scripts/ScriptInstall.sh
+++ Scripts/ScriptInstall.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "========================================================"
-echo "ATUALIZANDO O SISTEMA"
+echo "UPDATING SYSTEM"
 echo "========================================================"
 
 sudo apt update && sudo apt upgrade -y
 
 echo "========================================================"
-echo "INSTALAÇÃO DE PROGRAMAS ESSENCIAIS"
+echo "INSTALLING ESSENTIAL PROGRAMS"
 echo "========================================================"
 
 sudo apt install git gparted vim gcc wget g++ htop vlc gimp openjdk-25-jdk libreoffice texlive-full texmaker -y 
 
 echo "========================================================"
-echo "INSTALAÇÃO GOOGLE CHROME"
+echo "INSTALLING GOOGLE CHROME"
 echo "========================================================"
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
@@ -23,7 +23,7 @@ sudo apt install ./google-chrome-stable_current_amd64.deb -y
 sudo rm google-chrome-stable_current_amd64.deb
 
 echo "========================================================"
-echo "CONFIGURAÇÃO GITHUB (PARA NÃO PEDIR SENHA)"
+echo "GITHUB CONFIGURATION"
 echo "========================================================"
 
 (type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
